9ca93e3d039c9d70455bbbb8ebb6e93996484522
kmisiunas <k.misiunas@gmail.com>
2017-01-29 10:08:36 +0000

    small fixes

--- app/maths/Cashed.scala
+++ app/maths/Cashed.scala
@@ -4,7 +4,7 @@ object Cashed {
 
   // get it from Mathematica
   private val map: Map[(String, Int), (String, List[String])] = Map(
-    ("kmisiunas", 1) -> ("kmisiunas-1.png",
+    ("kmisiunas", 1) -> ("images/kmisiunas-1.png",
     List(
       "Coffee increases your daily energy",
       "Alchol decreases your daily energy"
